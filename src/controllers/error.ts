b9bfd94b0c1cc0f70b6005ed32b61a4b3b4de997
Sunday Etom Eni <sundaywht@gmail.com>
2025-12-08 19:40:56 +0100

    add logs to all error and .env example file

--- src/controllers/error.ts
+++ src/controllers/error.ts
@@ -1,20 +1,22 @@
 import { Request, Response, NextFunction } from "express-serve-static-core";
 
 export const get404 = (req: Request, res: Response, next: NextFunction) => {
+    console.log(req);
     return res.status(404).json({
         status: false,
-        statusCode: 404, 
+        statusCode: 404,
         message: 'Endpoint url resource Not Found.'
     });
 }
 
-export const get500 = (error: any, req:Request, res: Response, next: NextFunction) => {
+export const get500 = (error: any, req: Request, res: Response, next: NextFunction) => {
+    console.log(error);
     const data = error.data;
     const statusCode = error.statusCode || 500;
 
     return res.status(statusCode).json({
         status: false,
-        statusCode: statusCode, 
+        statusCode: statusCode,
         message: error.message,
         data: data
     });
